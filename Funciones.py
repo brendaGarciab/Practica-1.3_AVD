2cb6e2e777a5198de91afafd2e8cfc1b82f85d2b
brendaGarciab <125430328+brendaGarciab@users.noreply.github.com>
2023-09-24 14:53:39 -0600

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -49,3 +49,52 @@ set_b = [4, 5, 6]
 distance = jaccard_distance(set_a, set_b)
 print("Distancia de Jaccard entre los conjuntos:", distance)
 '''
+
+###VECTORIZAR QUERY Y REGISTRO
+def vectorize(query, df, record_index):
+ 
+    # Obtener el registro específico del DataFrame en el índice dado
+    record = df.iloc[record_index]
+    query = query.lower()
+
+    # Combinar las columnas relevantes en una sola cadena 
+    
+    #combined_text = str(record['Title']) + " " + str(record['Genres']) + " " + str(record['Actor1']) + " " + str(record['Actor2']) + " " + str(record['Actor3']) + " " + str(record['Director'])
+    combined_text = str(record['Title']).lower() + " " + str(record['Genres']).lower() + " " + str(record['Actor1']).lower() + " " + str(record['Actor2']).lower() + " " + str(record['Actor3']).lower() + " " + str(record['Director']).lower()
+
+
+    # Tokenizar la consulta del usuario y el registro
+    query_tokens = query.split()
+    record_tokens = combined_text.split()
+
+    # Crear un conjunto de todas las palabras únicas en la consulta y el registro
+    all_words = set(query_tokens).union(set(record_tokens))
+
+    # Función para vectorizar un texto en función de las palabras únicas
+    def vectorize_text(text_tokens, unique_words):
+        vector = [1 if word in text_tokens else 0 for word in unique_words]
+        return np.array(vector)
+
+    # Vectorizar la consulta del usuario y el registro
+    query_vector = vectorize_text(query_tokens, all_words)
+    record_vector = vectorize_text(record_tokens, all_words)
+
+    return query_vector, record_vector
+
+# Ejemplo de vectorizar
+'''
+data = {'Title': ["Movie 1", "Movie 2", "Movie 3"],
+        'Genres': ["action comedy romance", "comedy drama", "action adventure"],
+        'Actor1': ["Chris Evans", "Tom Hiddleston", "Tom Ellis"],
+        'Actor2': ["David Tennant", "Jeff Goldblum", "Richard Gere"],
+        'Actor3': ["Ryan Reynolds", "Tom Cavanagh", "Penn Badgley"],
+        'Director': ["Christopher Nolan", "Peter Jackson", "Martin Scorsese"]}
+df = pd.DataFrame(data)
+
+user_query = "I want a comedy with drama"
+record_index = 2  # Índice del registro en el DataFrame
+
+query_vector, record_vector = vectorize(user_query, df, record_index)
+print("Vector de la consulta:", query_vector)
+print("Vector del registro en el índice", record_index, ":", record_vector)
+'''
